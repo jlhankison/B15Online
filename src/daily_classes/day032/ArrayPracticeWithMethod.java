eb402c5e28fad7bf5737d4840328d2581680bae8
jlhankison <jlhankison@gmail.com>
2019-12-22 14:35:26 -0600

    Added a test for arrayPrinter

--- src/daily_classes/day032/ArrayPracticeWithMethod.java
+++ src/daily_classes/day032/ArrayPracticeWithMethod.java
@@ -1,4 +1,22 @@
 package daily_classes.day032;
 
 public class ArrayPracticeWithMethod {
+
+    public static void arrayPrinter (int [] nums){
+
+        for (int num :
+                nums){
+            System.out.print(num);
+        }
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] nums = {34, 3, 1, 343,6,8,43};
+
+        arrayPrinter(nums);
+    }
 }
