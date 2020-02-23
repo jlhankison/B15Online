67f10d915142fc2d256a390f42d8c8b22c238f86
jlhankison <jlhankison@gmail.com>
2020-02-23 16:16:07 -0600

    Day 64: LinkedHashMap, SortedMap, TreeMap, Queue, FIFO Introduction

---
+++ src/daily_classes/day064/Deque_for_LIFO.java
@@ -0,0 +1,25 @@
+package daily_classes.day064;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Deque_for_LIFO {
+
+    public static void main(String[] args) {
+
+        Deque<String> lifoQue = new LinkedList<>();
+
+        lifoQue.add("Review the Class");
+        lifoQue.add("do your homework");
+        lifoQue.add("Attend the class");
+        lifoQue.add("Say bye to Java");
+
+        System.out.println("taskQueue = " + lifoQue);
+
+
+        System.out.println("taskQueue.removeLast() = " + lifoQue.removeLast());
+        System.out.println("taskQueue.removeLast() = " + lifoQue.removeLast());
+        System.out.println("taskQueue.removeFirst() = " + lifoQue.removeFirst());
+        System.out.println("taskQueue.removeFirst() = " + lifoQue.removeFirst());
+    }
+}
